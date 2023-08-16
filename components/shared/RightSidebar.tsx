5d567c31c65f9f34b67be578b8e5a4da3cdf3a78
Michael Ruhl <michaelruhl14@hotmail.com>
2023-08-16 13:31:46 -0400

    create navbars

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function RightSidebar() {
+    return (
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3>Suggested Communities</h3>
+            </div>
+        </section>
+    )
+}
+
+export default RightSidebar;
