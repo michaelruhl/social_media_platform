205f2bd1f20942596714317c6f86250ddce399fd
Michael Ruhl <michaelruhl14@hotmail.com>
2023-08-17 19:57:20 -0400

    implement shadcn

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import {dark} from "@clerk/themes"
 
 function Topbar() {
   return (
@@ -34,7 +35,8 @@ function Topbar() {
         appearance={{
             elements: {
                 organizationSwitcherTrigger: "py-2 px-4"
-            }
+            },
+            baseTheme: dark,
         }}
         />
       </div>
