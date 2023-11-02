ed15f79bfbb28558a83b0b3edf8fdabcb65fb8df
Michael Ruhl <michaelruhl14@hotmail.com>
2023-11-02 15:36:05 -0400

    implement activity tab

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -6,16 +6,19 @@ import {usePathname, useRouter} from 'next/navigation'
 
 import { sidebarLinks } from "@/constants";
 import Image from "next/image";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
 function LeftSidebar() {
     const router = useRouter()
-    const pathname = usePathname()          
+    const pathname = usePathname()      
+    const {userId} = useAuth()      
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
             const isActive = (pathname.includes(link.route) && link.route.length > 1) || (pathname === link.route)
+
+            if(link.route === '/profile') link.route = `${link.route}/${userId}`
           return (
             <Link
               href={link.route}
