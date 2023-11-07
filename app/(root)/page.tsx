72f7c63005d3259c5dd7add1458cb8f2f0a288d3
Michael Ruhl <michaelruhl14@hotmail.com>
2023-11-06 19:15:24 -0500

    fix home feed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
   if (!user) return null;
   return (
     <>
-    {console.log(result.posts)}
+    
       <h1 className="head-text text-left">Home</h1>
 
       <section className="mt-9 flex flex-col gap-10">
